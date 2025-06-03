34f0d4cdba63527a6459531e80c88a84724e9e0a
Devashish-10 <121618997+Devashish-10@users.noreply.github.com>
2025-06-03 20:29:29 +0530

    emotion_detection_streamlit_App.py

--- emotion_detection_streamlit_App.py
+++ emotion_detection_streamlit_App.py
@@ -49,7 +49,7 @@ with tab1:
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file).convert("RGB")
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
         label = predict_emotion(image)
         st.markdown(f"### Prediction: `{label}`")
 
